d01b68daa32eeadf031b715144a9498627bdd75b
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-10 22:45:36 +0800

    refactor: logging to a function, also log the discarded event

--- waylandmap/keymapper.py
+++ waylandmap/keymapper.py
@@ -8,6 +8,21 @@ from waylandmap.constants import KEYS_VALUE_TUPLE, code_to_name
 from waylandmap.devices import get_device_path
 
 
+def log_input_output(input, output=None):
+    type_in, code_in, value_in = input
+    name_in = code_to_name(code_in) if type_in == 1 else ''
+    if output is not None:
+        (type_out, code_out), value_out = output
+        name_out = code_to_name(code_out) if type_out == 1 else ''
+    else:
+        # if discarded by filter
+        name_out = type_out = code_out = value_out = ''
+    # log
+    logging.info((
+        f'\tIN: ({name_in:>15},T={type_in:1},C={code_in:3},V={value_in:6}), '
+        f'\tOUT: ({name_out:>15},T={type_out:1},C={code_out:3},V={value_out:6})'))
+
+
 def infinite_retry(sleep, catch):
     def wrapper(func):
         def wrapped(*args, **kwargs):
@@ -36,22 +51,19 @@ def run(dev_name, keymaps):
             # loop to capture every event
             for ev in kb.read_loop():
                 # each event represented by three ints
-                type_in, code_in, value_in = ev.type, ev.code, ev.value
+                type_in, code_in, value_in = input = ev.type, ev.code, ev.value
                 # ask filter to for correct mapping
-                result = filter.target(type_in, code_in, value_in)
+                output = filter.target(type_in, code_in, value_in)
                 # this key is a registered modifier key
-                if result is None:
+                if output is None:
                     # drop the event and move on to next
+                    log_input_output(input)
                     continue
                 # emit the event to OS
-                vdev.emit(*result)
+                vdev.emit(*output)
                 # log
-                (type_out, code_out), value_out = result
-                name_in = code_to_name(code_in) if type_in == 1 else ''
-                name_out = code_to_name(code_out) if type_out == 1 else ''
-                logging.info((
-                    f'\tIN: ({name_in:>15},T={type_in:1},C={code_in:3},V={value_in:6}), '
-                    f'\tOUT: ({name_out:>15},T={type_out:1},C={code_out:3},V={value_out:6})'))
+                log_input_output(input, output)
+
     except KeyboardInterrupt:
         kb.ungrab()
         logging.error('User keyboard interrupted, quitting now.')
