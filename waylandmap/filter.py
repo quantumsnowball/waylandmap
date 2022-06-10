16137eff5aec6d24f11f6b89777f03a6b6e9c62a
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-11 04:41:42 +0800

    debug: if modifier key was released before the source key, the repeating action would not stop. Now filter will check if a combo path was turn on regardless of the status of modifier key. This should fix the bug.

--- waylandmap/filter.py
+++ waylandmap/filter.py
@@ -14,28 +14,32 @@ class Filter:
                           for m in keymaps if m['type'] == 'map')
         self._combo = tuple((ntc(m['modifier']), ntc(m['source']), ntc(m['target']))
                             for m in keymaps if m['type'] == 'combo')
-        self._on_mods = {k[0]:False for k in self._combo}
-        # self._on_remap_path = {c:False for c in self._combo}
+        # modifier keys states
+        self._on_modifier = {k[0]:False for k in self._combo}
+        # mapping path states
+        self._on_combo = {c:False for c in self._combo}
 
     def target(self, type_in: int, code_in: int, value_in: int) -> tuple | None:
         type_out, code_out, value_out = type_in, code_in, value_in
         # only interested in key event, ignore sync event
         if type_in == EV_KEY:
             # check if input is a modifier key
-            if code_in in self._on_mods:
+            if code_in in self._on_modifier:
                 # if confirm, change on_mods state
-                self._on_mods[code_in] = value_in >= 1
+                self._on_modifier[code_in] = value_in >= 1
                 # then issue discard signal
                 return None
             # check all registered mods tree and trigger
             for remap_path in self._combo:
                 modifier, source, target = remap_path
-                # check if modifier is on
-                if self._on_mods[modifier]:
+                # check if modifier is on, or on_mapping flag is on
+                if self._on_modifier[modifier] or self._on_combo[remap_path]:
                     # check if key pressed matched source
                     if code_in == source:
                         # change output target accordingly
                         code_out = target
+                        # mark remap path flag to on, only turn off upon key release event
+                        self._on_combo[remap_path] = value_in >= 1
                         # only need to match the first path
                         break
         # unless is a registered modifier key, always return a valid result
